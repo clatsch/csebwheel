80e2fddd43d8d180866c03b028d17ba550223bb0
Cla Tschenett <cla@aimara.ch>
2023-02-24 17:01:58 +0100

    rainbow removed

--- spin.py
+++ spin.py
@@ -83,5 +83,5 @@ while True:
         # delay between updates
         time.sleep(0.1)
 
-finally:
-    pixels.fill((0, 0, 0))
+pixels.fill((0, 0, 0))  # turn off all pixels
+pixels.show()           # update the pixels to turn them off
