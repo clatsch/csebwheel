7095307ffbc1809c80b9f59e292b72953007e4aa
Cla Tschenett <cla@aimara.ch>
2023-02-25 08:39:12 +0100

    rotation added

--- rotation.py
+++ rotation.py
@@ -16,34 +16,13 @@ pixels = neopixel.NeoPixel(
 
 while True:
 
-    # for led in range(num_pixels): # start single rotation loop
+    rotations = 3
 
-    # pixels.fill((0, 0, 0))  # turn off all pixels
-    # pixels.show()
-    # time.sleep(1)
-    # pixels.fill((255, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
-    # pixels.fill((0, 255, 0))
-    # pixels.show()
-    # time.sleep(1)
+        for rotation in range(rotations):
 
-    # pixels.fill((255, 255, 0))
-    # time.sleep(1)
-    # pixels.fill((255, 0, 0))
-    # time.sleep(1)
-    # pixels.fill((0, 250, 250))
-    # time.sleep(1)
-    for i in range(num_pixels):
-        pixels[i] = (0, 0, 250)
-        time.sleep(1)
-        pixels[i] = (0, 0, 0)
-        print(i)
-
-    # time.sleep(1)pixels.show()
-    #     # time.sleep(1)
-
-
-
-    # pixel_index = random.randint(0, num_pixels-1)
+            for i in range(num_pixels):
+                pixels[i] = (0, 0, 250)
+                time.sleep(0.2)
+                pixels[i] = (0, 0, 0)
+                print(i)
 
