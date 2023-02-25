75a513c16955073f179178075954624fcde293ef
Cla Tschenett <cla@aimara.ch>
2023-02-25 08:27:10 +0100

    rotation added

--- rotation.py
+++ rotation.py
@@ -11,7 +11,7 @@ num_pixels = 16
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.2, auto_write=True, pixel_order=ORDER
 )
 
 while True:
@@ -35,16 +35,12 @@ while True:
     # pixels.fill((0, 250, 250))
     # time.sleep(1)
     pixels[1] = (0, 0, 250)
-    pixels.show()
     time.sleep(1)
     pixels[1] = (0, 0, 0)
-    pixels.show()
 
     pixels[2] = (255, 0, 250)
-    pixels.show()
     time.sleep(1)
     pixels[2] = (0, 0, 0)
-    pixels.show()
 
     # time.sleep(1)pixels.show()
     #     # time.sleep(1)
