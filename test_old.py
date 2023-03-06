7a2361d58b4f11c623b6a077216992d116f88618
Cla Tschenett <cla@aimara.ch>
2023-03-06 20:29:53 +0100

    xyz added

--- test_old.py
+++ test_old.py
@@ -1,34 +1,21 @@
-import skywriter
 import signal
+import skywriter
 
-max_x, max_y, max_z = 0, 0, 0
-min_x, min_y, min_z = 65535, 65535, 65535
-
-print('Skywriter is up and running')
-
-def xyz(x, y, z):
-    print("x:{0}, y:{1}, z:{2}".format(x, y, z))
-
-skywriter.onXYZ(xyz)
-
-@skywriter.move()
-def move(x, y, z):
-    print("Movement detected: x={}, y={}, z={}".format(x, y, z))
-
-@skywriter.airwheel()
-def airwheel(delta):
-    print(f"Airwheel: {delta}")
-
-@skywriter.double_tap()
-def doubletap(position):
-    print(f"Double tap: {position}")
-
-@skywriter.tap()
-def tap(position):
-    print(f"Tap: {position}")
+@skywriter.flick()
+def flick(start,finish):
+    print('Got flick!', start, finish)
 
 @skywriter.touch()
 def touch(position):
-    print(f"Touch: {position}")
+    print('Got touch!', position)
+
+@skywriter.airwheel()
+def spinny(delta):
+    print('Got a turn!', delta)
+
+@skywriter.move()
+def move(x, y, z):
+    print('Got a move!', x, y, z)
 
 signal.pause()
+
