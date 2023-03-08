75e20b7e1cb404fe00a16a8fc884410c4340e2b5
Cla Tschenett <cla@aimara.ch>
2023-03-08 11:49:19 +0100

    button added

--- rainbow.py
+++ rainbow.py
@@ -1,21 +1,10 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple test for NeoPixels on Raspberry Pi
 import time
 import board
 import neopixel
 
 
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
-
-# The number of NeoPixels
 num_pixels = 16
 
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
@@ -24,8 +13,6 @@ pixels = neopixel.NeoPixel(
 
 
 def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
     if pos < 0 or pos > 255:
         r = g = b = 0
     elif pos < 85:
@@ -55,25 +42,4 @@ def rainbow_cycle(wait):
 
 
 while True:
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((255, 0, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
-    #
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 255, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
-    #
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 0, 255, 0))
-    # pixels.show()
-    # time.sleep(1)
-
     rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
