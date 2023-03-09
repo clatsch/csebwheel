7489a46ff75c16089fc6c8c9c130de8d1709e334
Cla Tschenett <cla@aimara.ch>
2023-03-09 12:07:58 +0100

    presentation added

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ try:
     while True:
         select_spin = GPIO.input(17)
         select_idle_mode = GPIO.input(27)
-        # select_presentation_mode = GPIO.input(22)
+        select_presentation_mode = GPIO.input(22)
         if select_spin == False:
             print('Spin selected')
             start_spin()
         if select_idle_mode == False:
             print('Idle Mode Selected')
             start_idle_mode()
-        # if select_presentation_mode == False:
-        #     print('Presentation Mode Selected')
-        #     light_up_group()
-        #     time.sleep(0.2) # wait for button debounce
+        if select_presentation_mode == False:
+            print('Presentation Mode Selected')
+            light_up_group()
+            time.sleep(0.2) # wait for button debounce
 
 finally:
     GPIO.cleanup()
