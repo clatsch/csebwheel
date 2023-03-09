423e745e4e9f56c3393cef24e4abc9e6ec4a4d36
Cla Tschenett <cla@aimara.ch>
2023-03-09 11:25:39 +0100

    presentation added

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import time
 import RPi.GPIO as GPIO
 from spin import start_spin
 from idle import start_idle_mode
+from presentation import light_up_group
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(27, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
 
 try:
     while True:
         select_spin = GPIO.input(17)
         select_idle_mode = GPIO.input(27)
+        select_presentation_mode = GPIO.input(22)
         if select_spin == False:
             print('Spin selected')
             start_spin()
         if select_idle_mode == False:
-            print('Presentation selected')
+            print('Idle Mode Selected')
             start_idle_mode()
+        if select_presentation_mode == False:
+            print('Presentation Mode Selected')
+            light_up_group()
 
 
 finally:
---
+++ presentation.py
@@ -0,0 +1,33 @@
+import time
+import board
+import neopixel
+import RPi.GPIO as GPIO
+
+# Pin definitions
+BUTTON_PIN = 22
+pixel_pin = board.D18
+numleds = 16
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, numleds, brightness=0.2, auto_write=False, pixel_order=ORDER)
+
+# Define groups of pixels
+groups = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15]]
+
+# Initialize button
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
+
+# Define function to light up a group of pixels
+def light_up_group(group):
+    for i in group:
+        pixels[i] = (255, 255, 255) # Set color of pixels to white
+    pixels.show()
+
+# Main loop
+current_group = 0
+while True:
+    if GPIO.input(BUTTON_PIN) == False:
+        light_up_group(groups[current_group]) # Light up the current group
+        current_group = (current_group + 1) % len(groups) # Move to the next group
+        while GPIO.input(BUTTON_PIN) == False:
+            pass # Wait for button to be released
