e7148a1fad0d76d6930b4ee0bb3c9d131dbe22f2
Cla Tschenett <cla@aimara.ch>
2023-03-09 12:17:03 +0100

    callback added

--- presentation.py
+++ presentation.py
@@ -13,34 +13,31 @@ pixels = neopixel.NeoPixel(pixel_pin, numleds, brightness=0.2, auto_write=False,
 # Define groups of pixels
 groups = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15]]
 
-# # Initialize button
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(BUTTON_PIN, GPIO.IN)
-
 # Define function to light up a group of pixels
 def light_up_group(group):
+    # Turn off all pixels first
+    pixels.fill((0, 0, 0))
+    # Set the color of the pixels in the group to white
     for i in group:
-        pixels[i] = (255, 255, 255) # Set color of pixels to white
+        pixels[i] = (255, 255, 255)
     pixels.show()
 
-# Define function to switch off a group of pixels
-def switch_off_group(group):
-    for i in group:
-        pixels[i] = (0, 0, 0) # Set color of pixels to black
-    pixels.show()
+# Define a callback function to handle button presses
+def button_callback(channel):
+    global current_group
+    # Increment current_group to move to the next group
+    current_group = (current_group + 1) % len(groups)
+    light_up_group(groups[current_group])
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Add event detection for button press
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200)
 
 # Main loop
 current_group = 0
-
-def start_presentation_mode():
-    global current_group
-    switch_off_group(groups[(current_group - 1) % len(groups)]) # Switch off the previous group
-    light_up_group(groups[current_group]) # Light up the current group
-    current_group = (current_group + 1) % len(groups) # Move to the next group
-    while True:
-        if GPIO.input(BUTTON_PIN) == False:
-            switch_off_group(groups[(current_group - 1) % len(groups)]) # Switch off the previous group
-            light_up_group(groups[current_group]) # Light up the current group
-            current_group = (current_group + 1) % len(groups) # Move to the next group
-            while GPIO.input(BUTTON_PIN) == False:
-                pass # Wait for button to be released
+light_up_group(groups[current_group])
+while True:
+    pass  # Main loop does nothing
