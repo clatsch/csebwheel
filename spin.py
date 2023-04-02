a6dcdea44ce31ecd30fa20a6269bc972b4293f4c
Cla <design@clatschenett.com>
2023-04-02 07:55:44 +0200

    speed factor

--- spin.py
+++ spin.py
@@ -37,7 +37,7 @@ def start_spin():
     numleds = 363
     decay = rotations * numleds
     spin += 1
-    speed_factor = 5  # Increase this value to make the spinning faster
+    speed_factor = 100  # Increase this value to make the spinning faster
 
     for rotation in range(1, rotations):
         led_colour = (0, 0, 255)
