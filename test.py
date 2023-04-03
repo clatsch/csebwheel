c033f5f5f86be66a82b51d0fc4acbbcd8e75ba5f
Cla Tschenett <cla@aimara.ch>
2023-04-03 21:28:16 +0200

    now approach

--- test.py
+++ test.py
@@ -18,20 +18,12 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def start_spin():
     strength = random.uniform(0.4, 1.0)
-    rotations = int(random.randint(min_rotations, max_rotations) * strength * num_leds)
+    print(strength)
+    rotations = int(random.randint(min_rotations, max_rotations) * num_leds * strength)
     total_steps = rotations * 2
+    speed = 0.005 / strength
 
-    # Calculate initial speed based on strength and friction
-    friction = 0.95
-    speed = 10 * strength
-
-    # Spin the wheel
     for i in range(total_steps):
-        # Calculate current speed based on remaining distance and friction
-        remaining_steps = total_steps - i
-        current_speed = speed * remaining_steps / total_steps * friction
-
-        # Update LED colors and show them
         for j in range(5):
             prev_index = (i - 5 + j) % num_leds
             pixels[prev_index] = (0, 0, 0)
@@ -42,25 +34,13 @@ def start_spin():
 
         pixels.show()
 
-        # Wait for a short time based on current speed
-        delay_time = 0.001 / current_speed
+        progress = i / total_steps
+        delay_factor = random.uniform(0.8, 1.2)  # Random factor to introduce randomness to the speed reduction
+        delay_time = speed * (1 - progress) * delay_factor
         time.sleep(delay_time)
 
-    # Determine stopping point based on strength
-    stop_index = (rotations * strength * num_leds) % num_leds
-
-    # Turn off all LEDs and show them
     pixels.fill((0, 0, 0))
     pixels.show()
-
-    # Light up the stopping point in yellow and wait for a short time before finishing
-    for i in range(10):
-        if i % 2 == 0:
-            pixels[int(stop_index)] = (255, 255, 0)
-        else:
-            pixels[stop_index] = (0, 0, 0)
-        pixels.show()
-        time.sleep(0.1)
 
 while True:
     input_state = GPIO.input(button_pin)
