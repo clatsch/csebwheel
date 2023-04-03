3676c87a5a052abcd6a9cfc4e6f1cad41cf463a9
Cla Tschenett <cla@aimara.ch>
2023-04-03 21:17:18 +0200

    now approach

--- test.py
+++ test.py
@@ -17,22 +17,35 @@ button_pin = 17
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def start_spin():
+    # Generate a random strength between 40% and 100%
     strength = random.uniform(0.4, 1.0)
-    max_steps = random.randint(min_rotations, max_rotations) * num_leds * strength
-    current_step = 0
-    speed = 0.005 / strength
-    deceleration = 0.01 * (1 - strength)
+    # Calculate the number of rotations based on the strength and the min/max rotations
+    rotations = int(random.randint(min_rotations, max_rotations) * num_leds * strength)
+    # Calculate the total number of steps
+    total_steps = rotations * num_leds
+    # Calculate the initial delay time (faster initial speed)
+    delay_time = 0.0005 / strength
 
-    while current_step < max_steps:
+    # Loop through all the steps
+    for i in range(total_steps):
+        # Calculate the index of the first LED to light up
+        start_index = i % num_leds
+        # Light up the next 5 LEDs
         for j in range(5):
-            index = (current_step + j) % num_leds
+            index = (start_index + j) % num_leds
             pixels[index] = (0, 0, 255)
-
+        # Turn off the LEDs that were lit up in the previous iteration
+        for j in range(5):
+            index = (start_index - 5 + j) % num_leds
+            pixels[index] = (0, 0, 0)
+        # Show the updated LED colors
         pixels.show()
-        current_step += 1
-        time.sleep(speed)
-        speed += deceleration
+        # Gradually increase the delay time to simulate friction and slowing down
+        delay_time += 0.0000001 * (total_steps - i) / total_steps
+        # Wait for the calculated delay time
+        time.sleep(delay_time)
 
+    # Turn off all the LEDs
     pixels.fill((0, 0, 0))
     pixels.show()
 
