6387adc51df84e143c3987ddc46a089062f71bb0
Cla Tschenett <cla@aimara.ch>
2023-04-04 09:30:37 +0200

    spin_action() added

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+spin_mode = False
+
 try:
     while True:
         select_spin = GPIO.input(17)
@@ -16,17 +18,23 @@ try:
         select_presentation_mode = GPIO.input(22)
         print(f"Spin: {select_spin}, Idle: {select_idle_mode}, Presentation: {select_presentation_mode}")
 
-        if select_spin == False:
+        if select_spin == False and not spin_mode:
             print('Spin selected')
             start_spin()
-            spin_action()
+            spin_mode = True
             time.sleep(0.2)
+        elif spin_mode:
+            spin_action()
+
         if select_idle_mode == False:
             print('Idle Mode Selected')
+            spin_mode = False
             start_idle_mode()
             time.sleep(0.2)
+
         if select_presentation_mode == False:
             print('Presentation Mode Selected')
+            spin_mode = False
             light_up_group()
             time.sleep(0.2)
 
