5e0b98621d4099a75388ea7083b95a1d106828b5
Cla Tschenett <cla@aimara.ch>
2023-04-04 10:19:56 +0200

    debounce

--- main.py
+++ main.py
@@ -9,32 +9,35 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+DEBOUNCE_TIME = 0.3  # Set the debounce time to 300ms
+
+last_spin_time = 0
+last_idle_time = 0
+last_presentation_time = 0
+
 try:
     while True:
-        select_spin = GPIO.input(17)
-        select_idle_mode = GPIO.input(27)
-        select_presentation_mode = GPIO.input(22)
-        print(f"Spin: {select_spin}, Idle: {select_idle_mode}, Presentation: {select_presentation_mode}")
+        current_time = time.time()
 
-        if select_spin == False:
+        if GPIO.input(17) == False and current_time - last_spin_time > DEBOUNCE_TIME:
             print('Spin selected')
             start_spin()
-            time.sleep(0.2)
-        if select_idle_mode == False:
+            last_spin_time = current_time
+
+        if GPIO.input(27) == False and current_time - last_idle_time > DEBOUNCE_TIME:
             print('Idle Mode Selected')
             start_idle_mode()
-            time.sleep(0.2)
-        if select_presentation_mode == False:
+            last_idle_time = current_time
+
+        if GPIO.input(22) == False and current_time - last_presentation_time > DEBOUNCE_TIME:
             print('Presentation Mode Selected')
             light_up_group()
-            time.sleep(0.2)
+            last_presentation_time = current_time
 
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("All LEDs OFF")
 
-# Add this code after the while loop
 if any(pixels):
     pixels.fill((0, 0, 0))
     pixels.show()
-
