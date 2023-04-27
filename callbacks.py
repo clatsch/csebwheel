2884a9700840e830675be73acc73cb33514b2251
Cla <design@clatschenett.com>
2023-04-27 11:07:35 +0200

    newSpin added

--- callbacks.py
+++ callbacks.py
@@ -3,10 +3,10 @@ import time
 from spin import start_spin
 
 
-def spin_button_pressed(button_pin):
-    start_spin(button_pin)
-
-def button_callback(channel):
-    print("Button pressed")
-    start_spin(18)
+# def spin_button_pressed(button_pin):
+#     start_spin(button_pin)
+#
+# def button_callback(channel):
+#     print("Button pressed")
+#     start_spin(18)
 
