a50d28323f7d60ab5118adf7b623123aca3ef911
Cla <design@clatschenett.com>
2023-04-28 10:36:14 +0200

    speed from 0.01 to 0.1

--- idle.py
+++ idle.py
@@ -37,7 +37,7 @@ def start_idle_mode():
     global pixels
     rainbow_on = True
     while rainbow_on:
-        rainbow_cycle(0.01)  # Increase the wait time for a slower cycle
+        rainbow_cycle(0.1)  # Increase the wait time for a slower cycle
         if GPIO.input(27) == GPIO.LOW:
             rainbow_on = False
             pixels.fill((0, 0, 0, 0))
