8ca5638a3b9f06faa030f400585f22512355606b
Cla <design@clatschenett.com>
2023-04-28 09:59:23 +0200

    button press

--- idle.py
+++ idle.py
@@ -2,7 +2,6 @@ import time
 import board
 import neopixel
 import RPi.GPIO as GPIO
-from newSpin import start_spin
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -11,8 +10,6 @@ pixel_pin = board.D18
 numleds = 363
 ORDER = neopixel.RGBW
 pixels = neopixel.NeoPixel(pixel_pin, numleds, brightness=0.8, auto_write=False, pixel_order=ORDER)
-
-DEBOUNCE_TIME = 0.3  # Set the debounce time to 300ms
 
 def wheel(pos):
     if pos < 0 or pos > 255:
@@ -41,27 +38,23 @@ def rainbow_cycle(wait):
         pixels.show()
         time.sleep(wait)
 
-def start_idle_mode():
+def start_spin():
     global pixels
-    rainbow_on = True
+    spin_on = True
     last_button_time = 0  # initialize variable for last button press time
-    while rainbow_on:
+    while spin_on:
         rainbow_cycle(0.001)
         current_time = time.time()  # get current time
         if GPIO.input(27) == GPIO.LOW:
             # check if debounce time has passed since last button press
-            if current_time - last_button_time > DEBOUNCE_TIME:
-                rainbow_on = False
+            if current_time - last_button_time > 0.3:
+                spin_on = False
                 pixels.fill((0, 0, 0))
                 pixels.show()
                 while GPIO.input(27) == GPIO.LOW:
                     pass
                 last_button_time = current_time  # update last button press time
         elif GPIO.input(17) == GPIO.LOW:
-            # check if debounce time has passed since last button press
-            if current_time - last_button_time > DEBOUNCE_TIME:
-                rainbow_on = False
-                pixels.fill((0, 0, 0))
-                pixels.show()
-                start_spin()
-                last_button_time = current_time  # update last button press time
+            spin_on = False
+            pixels.fill((0, 0, 0))
+            pixels.show()
